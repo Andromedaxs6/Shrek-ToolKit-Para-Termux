4c0a3cbb6405e13f8673a5e2fe71900513a7db64
Andromedaxs6 <121472410+Andromedaxs6@users.noreply.github.com>
2022-12-27 20:38:41 -0300

    Add files via upload

---
+++ shrek.py
@@ -0,0 +1,56 @@
+import os
+os.system("apt install python3")
+os.system("apt install git")
+os.system("apt install figlet")
+print("""
+⢀⡴⠑⡄⠀⠀⠀⠀⠀⠀⠀⣀⣀⣤⣤⣤⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ 
+⠸⡇⠀⠿⡀⠀⠀⠀⣀⡴⢿⣿⣿⣿⣿⣿⣿⣿⣷⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀ 
+⠀⠀⠀⠀⠑⢄⣠⠾⠁⣀⣄⡈⠙⣿⣿⣿⣿⣿⣿⣿⣿⣆⠀⠀⠀⠀⠀⠀⠀⠀ 
+⠀⠀⠀⠀⢀⡀⠁⠀⠀⠈⠙⠛⠂⠈⣿⣿⣿⣿⣿⠿⡿⢿⣆⠀⠀⠀⠀⠀⠀⠀ 
+⠀⠀⠀⢀⡾⣁⣀⠀⠴⠂⠙⣗⡀⠀⢻⣿⣿⠭⢤⣴⣦⣤⣹⠀⠀⠀⢀⢴⣶⣆ 
+⠀⠀⢀⣾⣿⣿⣿⣷⣮⣽⣾⣿⣥⣴⣿⣿⡿⢂⠔⢚⡿⢿⣿⣦⣴⣾⠁⠸⣼⡿ 
+⠀⢀⡞⠁⠙⠻⠿⠟⠉⠀⠛⢹⣿⣿⣿⣿⣿⣌⢤⣼⣿⣾⣿⡟⠉⠀⠀⠀⠀⠀ 
+⠀⣾⣷⣶⠇⠀⠀⣤⣄⣀⡀⠈⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀ 
+⠀⠉⠈⠉⠀⠀⢦⡈⢻⣿⣿⣿⣶⣶⣶⣶⣤⣽⡹⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀ 
+⠀⠀⠀⠀⠀⠀⠀⠉⠲⣽⡻⢿⣿⣿⣿⣿⣿⣿⣷⣜⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀ 
+⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣷⣶⣮⣭⣽⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀ 
+⠀⠀⠀⠀⠀⠀⣀⣀⣈⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇⠀⠀⠀⠀⠀⠀⠀ 
+⠀⠀⠀⠀⠀⠀⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀ 
+⠀⠀⠀⠀⠀⠀⠀⠹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀ 
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⠻⠿⠿⠿⠿⠛⠉""")
+os.system("figlet Shrek Basic Toolkit")
+print("Created by ANDR0M3D4, Não Kibar Ok? =P")
+print()
+print("[1] Instalar SQLMap")
+print("[2] Instalar NMap")
+print("[3] Instalar TCPDump")
+print("[4] Instalar Metasploit")
+print("[5] Instalar Crunch")
+print("[6] Instalar WhoIs")
+print("[7] Instalar NetCat")
+opção = int(input("Escolha a ferramenta que deseja instalar"))
+if opção == 1:
+        os.system("git clone https://github.com/sqlmapproject/sqlmap")
+        os.system("python3 shrek.py")
+elif opção == 2:
+        os.system("apt install nmap")
+        os.system("python3 shrek.py")
+elif opção == 3:
+        os.system("git clone https://github.com/k1LoW/tcpdp")
+        os.system("python3 shrek.py")
+elif opção == 4:
+        os.system("git clone https://github.com/gushmazuko/metasploit_in_termux")
+        os.system("python3 shrek.py")
+elif opção == 5:
+        os.system("apt install crunch")
+        os.system("python3 shrek.py")
+elif opção == 6:
+        os.system("apt install whois")
+        os.system("python3 shrek.py")
+elif opção == 7:
+        os.system("git clone https://github.com/Juliocj7/NetcatCj7")
+        os.system("python3 shrek.py")
+else:
+        print("opção não encontrada, reiniciando o script...")
+        os.system("clear")
+        os.system("python3 shrek.py")
