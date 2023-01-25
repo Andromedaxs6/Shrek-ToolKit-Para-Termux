b51438934ca82e53e1b4be52facc3d8cd4192256
Andromedaxs6 <121472410+Andromedaxs6@users.noreply.github.com>
2023-01-25 08:39:10 -0300

    Add files via upload
    Kali-Linux-Version-xD

---
+++ shrek-kali.py
@@ -0,0 +1,54 @@
+import os
+os.system("clear")
+print("""                                                                               ⢀⡴⠑⡄⠀⠀⠀⠀⠀⠀⠀⣀⣀⣤⣤⣤⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠸⡇⠀⠿⡀⠀⠀⠀⣀⡴⢿⣿⣿⣿⣿⣿⣿⣿⣷⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠑⢄⣠⠾⠁⣀⣄⡈⠙⣿⣿⣿⣿⣿⣿⣿⣿⣆⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⢀⡀⠁⠀⠀⠈⠙⠛⠂⠈⣿⣿⣿⣿⣿⠿⡿⢿⣆⠀⠀⠀⠀⠀⠀⠀                                                          ⠀⠀⠀⢀⡾⣁⣀⠀⠴⠂⠙⣗⡀⠀⢻⣿⣿⠭⢤⣴⣦⣤⣹⠀⠀⠀⢀⢴⣶⣆                                                          ⠀⠀⢀⣾⣿⣿⣿⣷⣮⣽⣾⣿⣥⣴⣿⣿⡿⢂⠔⢚⡿⢿⣿⣦⣴⣾⠁⠸⣼⡿
+⠀⢀⡞⠁⠙⠻⠿⠟⠉⠀⠛⢹⣿⣿⣿⣿⣿⣌⢤⣼⣿⣾⣿⡟⠉⠀⠀⠀⠀⠀
+⠀⣾⣷⣶⠇⠀⠀⣤⣄⣀⡀⠈⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀
+⠀⠉⠈⠉⠀⠀⢦⡈⢻⣿⣿⣿⣶⣶⣶⣶⣤⣽⡹⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠉⠲⣽⡻⢿⣿⣿⣿⣿⣿⣿⣷⣜⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣷⣶⣮⣭⣽⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⣀⣀⣈⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⠻⠿⠿⠿⠿⠛⠉""")
+os.system("figlet Shrek Basic Toolkit")
+print("Created by ANDR0M3D4, Não Kibar Ok? =P")
+print()
+print("[1] Instalar SQLMap")
+print("[2] Instalar NMap")                                                              print("[3] Instalar TCPDump")
+print("[4] Instalar Metasploit")                                                        print("[5] Instalar Crunch")
+print("[6] Instalar WhoIs")
+print("[7] Instalar NetCat")
+print("[8] Sair do Script")
+opção = int(input("Escolha a ferramenta que deseja instalar: "))
+if opção == 1:
+        os.system("sudo apt install sqlmap")
+        os.system("python3 shrek.py")
+elif opção == 2:
+        os.system("sudo apt install nmap")
+        os.system("python3 shrek.py")
+elif opção == 3:
+        os.system("sudo git clone https://github.com/k1LoW/tcpdp")
+        os.system("python3 shrek.py")
+elif opção == 4:
+        os.system("sudo git clone https://github.com/gushmazuko/metasploit_in_termux")
+        os.system("python3 shrek.py")
+elif opção == 5:
+        os.system("sudo apt install crunch")
+        os.system("python3 shrek.py")
+elif opção == 6:
+        os.system("sudo apt install whois")
+        os.system("python3 shrek.py")
+elif opção == 7:
+        os.system("sudo apt install ncat")
+        os.system("python3 shrek.py")
+elif opção == 8:
+        os.system("exit")
+        os.system("clear")
+        os.system("cd")
+else:
+        print("opção não encontrada, reiniciando o script...")
+        os.system("clear")
+        os.system("python3 shrek.py")
